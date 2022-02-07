22b663df24988963a3f3f48c3017d496d8e1e0bd
jegnaw2021 <jegnawjegnaw@gmail.com>
2022-02-07 21:44:38 +0300

    fetching character data and displaing finished or started

--- src/components/characters.js
+++ src/components/characters.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Characters = () => {
+  const [characters, setCharacters] = useState([]);
+  const getAllCharacters = async () => {
+    const data = await fetch("https://rickandmortyapi.com/api/character").then(
+      (res) => res.json()
+    );
+    setCharacters(data.results);
+  };
+  useEffect(() => {
+    getAllCharacters();
+  }, []);
   return (
     <div>
-      <h2>characters</h2>
+      {characters.map((character) => {
+        return <h2 key={character.id}>{character.name}</h2>;
+      })}
     </div>
   );
 };
